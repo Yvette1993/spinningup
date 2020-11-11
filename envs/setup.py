5094cf291fa24cf93d58b4507dab56dafe73dac1
Yvette1993 <yingyinglee1993@163.com>
2020-11-11 14:36:38 +0800

    udate setup

--- envs/setup.py
+++ envs/setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
 
-setup(name='cell_place_gym',    # Secondary directory
+# setup(name='cell_place_gym',    # Secondary directory
+#     version='0.1',
+#     install_requires=['gym']    # And any other dependencies foo needs
+# )
+
+setup(name='chip_place_gym',    # Secondary directory
     version='0.1',
-    install_requires=['gym']    # And any other dependencies foo needs
+    #install_requires=['gym']    # And any other dependencies foo needs
 )
